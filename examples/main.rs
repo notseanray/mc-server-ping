286e3985aac3c150ed0b388cb173dfe7b5d241e3
NotCreative21 <seanray410@gmail.com>
2022-07-29 09:54:43 -0400

    correct example ip

--- examples/main.rs
+++ examples/main.rs
@@ -1,7 +1,7 @@
 use mc_server_ping::ServerStatus;
 
 fn main() {
-    let mut server = ServerStatus::new("ssh.hypnos.ws", 12001, None, None);
+    let mut server = ServerStatus::new("mc.hypixel.net", 25565, None, None);
     server.query().unwrap();
     let response = server.to_json().unwrap();
     println!("{}", serde_json::to_string_pretty(&response).unwrap());
